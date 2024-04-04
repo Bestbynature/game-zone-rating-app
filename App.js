fa21b23af08a8ea0131e5394c5a4143d6a0079a2
Dammy <bestbynature2010@gmail.com>
2024-04-04 01:22:16 +0100

    Update the views

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!</Text>
+      <View style={styles.header}>
+        <Text>Hello, World</Text>
+      </View>
+      {/* <Text>Open up App.js to start working on your app!!!!!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: '#f7287b',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
